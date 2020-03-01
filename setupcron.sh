d6311699b344d744e86be11451f33e0b36ab483e
raj.cs.psg@gmail.com <raj.cs.psg@gmail.com>
2020-03-01 01:49:37 -0500

    updated with vagrant file

---
+++ setupcron.sh
@@ -0,0 +1,9 @@
+sudo yum install epel-release -y
+sudo curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum install gcc-c++ make -y
+sudo yum install -y nodejs
+sudo yum install redhat-rpm-config -y
+sudo yum install rpm-build -y
+sudo yum install gcc -y
+sudo yum install make -y
+
